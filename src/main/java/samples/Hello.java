96244c0ead0d151f57728a1e72d56563e31d756e
Mohammad Aslam <aslamm@ncs.com.sg>
2015-03-30 10:58:19 +0800

    updated

---
+++ src/main/java/samples/Hello.java
@@ -0,0 +1,54 @@
+package samples;
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Simple Hello servlet.
+ */
+
+public final class Hello extends HttpServlet {
+
+
+    /**
+     * Respond to a GET request for the content produced by
+     * this servlet.
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are producing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+      throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>Sample Application Servlet Page</title>");
+        writer.println("</head>");
+        writer.println("<body bgcolor=white>");
+
+        writer.println("<table border=\"0\" cellpadding=\"10\">");
+        writer.println("<tr>");
+        writer.println("<td>");
+        writer.println("<h1>My Sample Servlet Application</h1>");
+        writer.println("</td>");
+        writer.println("</tr>");
+        writer.println("</table>");
+
+        writer.println("This response came from hello servlet which is part of AslamWebApp sample Application");
+        writer.println("");
+
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World This is a sample jsp test page!</h2>
+</body>
+</html>

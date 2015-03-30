7cc78fb7425aa462fa280c79d2697d1218ae5c12
Mohammad Aslam <aslamm@ncs.com.sg>
2015-03-30 16:11:30 +0800

    added 2

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@
 cd $HOME/$BUILD_HOME/AslamWebApp/target/
 cp AslamWebApp.war $HOME/$BUILD_HOME/AslamWebApp/build/
 cd $HOME/$BUILD_HOME/AslamWebApp/build
+scp -r $HOME/$BUILD_HOME/AslamWebApp/build/AslamWebApp.war ubuntu@ec2-52-74-74-62.ap-southeast-1.compute.amazonaws.com:/home/ubuntu/
 #git add .
 #git commit -a -m "updated"
 #git push origin master

70903d0f326483b153f9fb728800eef4252eb821
Mohammad Aslam <aslamm@ncs.com.sg>
2015-03-30 15:41:12 +0800

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
 ## this script will deploy the generated war file to a new ec2 server
 
+#echo "Start deploying ..."
+
+cd $HOME/$BUILD_HOME/AslamWebApp/target/
+cp AslamWebApp.war $HOME/$BUILD_HOME/AslamWebApp/build/
+git add .
+git commit -a -m "updated"
+git push origin master
+
+#echo "Upload done!"

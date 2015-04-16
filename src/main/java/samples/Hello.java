be0f2f11e53aef6a5d9ec575fa628790e23c0197
aslam <aslamm@ncs.com.sg>
2015-04-16 07:23:52 +0000

    updated 63

--- src/main/java/samples/Hello.java
+++ src/main/java/samples/Hello.java
@@ -45,7 +45,7 @@ public final class Hello extends HttpServlet {
         writer.println("</tr>");
         writer.println("</table>");
 
-        writer.println("This response came from hello servlet which is part of AslamWebApp sample Application..1");
+        writer.println("##### This response came from hello servlet which is part of AslamWebApp sample Application..2");
         writer.println("");
 
         writer.println("</body>");
